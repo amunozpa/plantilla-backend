bc975c5d3a4ee1bdf1992e748626f7722e7daf5c
amue muñoz <amue.munoz@culqi.com>
2021-03-05 16:38:28 -0500

    Se hizo envios desde el cliente a servidor y viceversa

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 //crear server http con express - fuente https://www.npmjs.com/package/express
 
 const express = require('express')
+//const bodyParser = require('body-parser'); //body parser se depreco ya que al parecer esta incluido en express
+
 //con router podemos separar rutas usando los verbos permitidos en peticiones http
 const router = express.Router();
 
 const app = express()
-
+app.use(express.json()); //alternativa a body-parser antes: bodyParser.json()
+app.use(express.urlencoded({extended: false}));
 app.use(router);
 
 router.get('/messages', function(req,res) {
+  console.log(req.query) // datos en la url (params)
+  console.log(req.body) //datos en el body
+  
   res.send('listado de datos');
+})
+
+router.delete('/messages', function(req,res) {
+  console.log(req.query) // datos en la url (params)
+  console.log(req.body) //datos en el body
+  res.send('Mensaje request ' + req.body.saludo+ ' '+ req.query.order); //esto manda algo al cliente en este caso un dato del request
 })
 
 router.post('/messages', function(req,res) {
