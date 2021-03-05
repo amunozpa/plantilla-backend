e13d03242c8206f3cbb591d31c3314b426db7a63
amue muñoz <amue.munoz@culqi.com>
2021-03-05 12:56:59 -0500

    crear servidor http

---
+++ server.js
@@ -0,0 +1,12 @@
+//crear server http con express - fuente https://www.npmjs.com/package/express
+
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send("hola")
+})
+ 
+app.listen(3000)
+
+console.log("el puerto a escuchar es http://localhost:3000");
