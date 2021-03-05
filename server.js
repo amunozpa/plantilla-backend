9279f7ea276b1c824744355de1cf42dbe34cc9fe
amue muñoz <amue.munoz@culqi.com>
2021-03-05 15:16:22 -0500

    se agrego router para separar llamadas

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 //crear server http con express - fuente https://www.npmjs.com/package/express
 
 const express = require('express')
+//con router podemos separar rutas usando los verbos permitidos en peticiones http
+const router = express.Router();
+
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.send("hola")
+
+app.use(router);
+
+router.get('/messages', function(req,res) {
+  res.send('listado de datos');
 })
+
+router.post('/messages', function(req,res) {
+  res.send('se ingreso el nuevo dato');
+})
+
+// app.use('/', function (req, res) {
+//   res.send("hola")
+// })
  
 app.listen(3000)
 
 console.log("el puerto a escuchar es http://localhost:3000");
 
 
-//comentario
